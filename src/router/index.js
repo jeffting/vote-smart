5ea5344d3123cee50e0220cec0121f14c7a6d23a
Eden Knudson <edenknudson@Edens-MacBook-Pro.local>
2020-02-05 21:16:02 -0700

    Added pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Issues from '@/components/Issues'
+import Quiz from '@/components/Quiz'
+import Candidates from '@/components/Candidates'
 
 Vue.use(Router)
 
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/Issues',
+      name: 'Issues',
+      component: Issues
+    },
+    {
+      path: '/Candidates',
+      name: 'Candidates',
+      component: Candidates
+    },
+    {
+      path: '/Quiz',
+      name: 'Quiz',
+      component: Quiz
     }
   ]
 })
