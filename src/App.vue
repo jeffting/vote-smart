686f1f080f6caa531e1e22faf0463e1701733264
Jeff Tingey <jeff@route.com>
2020-02-25 17:23:17 -0700

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <navbar></navbar>
-    <router-view/>
+    <transition name="fade"
+        mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
@@ -52,5 +55,16 @@ p {
     height: 120px;
     border-radius: 60px;
     object-fit: cover;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.2s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
 }
 </style>
