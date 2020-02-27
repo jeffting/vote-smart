c30ac070b578fba67cba2a665b6f761254890a48
Jeff Tingey <jeff@route.com>
2020-02-27 13:25:54 -0700

    added learn more

--- src/components/Issues.vue
+++ src/components/Issues.vue
@@ -6,22 +6,26 @@
                 <img class="issue-img-container" src="../assets/money.jpg">
                 <h3 class="issue-title">Economy</h3>
                 <p class="issue-info">The US economy is currently in its longest expansion ever, and unemployment is at a record low. That's music to President Trump's ears. But the country's also dealing with rising income inequality and stagnant wages.</p>
+                <p class="learn-more-issue">Learn More</p>
             </div>
             <div class="flex-item-row" v-on:click="issueClick('abortion')">
                 <img class="issue-img-container" src="../assets/pregnancy_test.jpg">
                 <h3 class="issue-title">Abortion</h3>
                 <p class="issue-info" >Abortion rates have been going down for decades. But with the recent wave of statewide abortion restrictions, the issue has taken a front seat in the 2020 election.</p>
+                <p class="learn-more-issue">Learn More</p>
             </div>
             <div class="flex-item-row" v-on:click="issueClick('climate')">
                 <img class="issue-img-container" src="../assets/sun.jpeg">
                 <h3 class="issue-title">Climate Change</h3>
                 <p class="issue-info">The effects of climate change - like more frequent wildfires and flooding - are hitting home for voters in a way they haven't in the past.</p>
+                <p class="learn-more-issue">Learn More</p>
             </div>
 
             <div class="flex-item-row" v-on:click="issueClick('education')">
                 <img class="issue-img-container" src="../assets/school.jpg">
                 <h3 class="issue-title">Education</h3>
                 <p class="issue-info">The nation has a $1.5 trillion student debt problem. That's led to a fierce debate about how to make college more affordable. The candidates also have policies to address everything from universal pre-k to charter schools.</p>
+                <p class="learn-more-issue">Learn More</p>
             </div>
         </div>
         <div class="row-flex" style="margin-top: 16px">
@@ -29,16 +33,19 @@
                 <img class="issue-img-container" src="../assets/gun.jpg">
                 <h3 class="issue-title">Guns</h3>
                 <p class="issue-info">A string of mass shootings in recent years have amplified this issue for American voters and ignited new calls for action by lawmakers.</p>
+                <p class="learn-more-issue">Learn More</p>
             </div>
             <div class="flex-item-row" v-on:click="issueClick('health')">
                 <img class="issue-img-container" src="../assets/health.jpg">
                 <h3 class="issue-title">Health Care</h3>
                 <p class="issue-info">US Health Care is the most expensive in the world. And Americans are fed up with its high prices, making it one of the most pressing issues on the campaign trail.</p>
+                <p class="learn-more-issue">Learn More</p>
             </div>
             <div class="flex-item-row" v-on:click="issueClick('immigration')">
                 <img class="issue-img-container" src="../assets/statue.jpg">
                 <h3 class="issue-title">Immigration</h3>
                 <p class="issue-info">Immigration has been a defining issue for President Trump's first term. That's fueled a debate about everything from the future of DACA to border security to refugee policies.</p>
+                <p class="learn-more-issue">Learn More</p>
             </div>
             <div class="flex-item-row" v-on:click="issueClick('security')">
                 <img class="issue-img-container" src="../assets/security.jpg">
@@ -46,6 +53,7 @@
                 <p class="issue-info">The threat of terrorism. Nuclear war. Cybersecurity. There are a wide variety of issues that could affect the safety and security of Americans.
 
 </p>
+                <p class="learn-more-issue">Learn More</p>
             </div>
         </div>
     </div>
@@ -66,6 +74,12 @@ export default {
 }
 </script>
 <style>
+.learn-more-issue {
+    color: #71b6ff;
+    text-decoration: underline;
+    text-align: left;
+    margin-top: 0px;
+}
 .col-flex {
     display: flex;
     flex-direction: column;
@@ -104,7 +118,7 @@ export default {
     margin-bottom: 0px;
 }
 .issue-info {
-    margin: 0px;
+    margin: 8px 0 0 0;
     text-align: left;
 }
 
